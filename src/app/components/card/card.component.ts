95355e38382071909445ca80cb2a8b72cc8bbd0b
Eleazar Braco Terry <ebracoterry@gmail.com>
2023-05-06 11:28:25 -0500

    feat: stores fix

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,27 +1,54 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { EmpAddEditComponent } from '../emp-add-edit/emp-add-edit.component';
 import { MatDialog } from '@angular/material/dialog';
+import { Store } from 'src/app/core/models/store.model';
+import { StoreService } from 'src/app/core/services/stores/store.service';
 
 @Component({
   selector: 'card-component',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+  @Input() store!: Store;
+  @Output() deletedStore = new EventEmitter();
 
-  constructor(private _dialog: MatDialog){}
+  constructor(
+    private _dialog: MatDialog,
+    private storeService: StoreService,
+    private cdRef: ChangeDetectorRef
+  ) {}
+  ngOnInit(): void {
+    console.log(this.store);
+  }
 
-  openEditForm(data = null) {
+  openEditForm() {
     const dialogRef = this._dialog.open(EmpAddEditComponent, {
-      data,
+      data: { store: this.store },
     });
 
     dialogRef.afterClosed().subscribe({
       next: (val) => {
         if (val) {
-          console.log(val);
+          this.store = val;
+          this.cdRef.detectChanges();
         }
       },
     });
+  }
+
+  deleteStore() {
+    let _confirm = confirm('Estas seguro de eliminar esta tienda?');
+    if (_confirm) {
+      this.storeService.delete(this.store.id).subscribe({});
+      this.deletedStore.emit();
+    }
   }
 }
--- src/app/modules/dashboards/stores/stores.component.ts
+++ src/app/modules/dashboards/stores/stores.component.ts
@@ -1,14 +1,25 @@
-import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
+import { HttpParams } from '@angular/common/http';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Input,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Subject } from 'rxjs';
 import { EmpAddEditComponent } from 'src/app/components/emp-add-edit/emp-add-edit.component';
+import { Store } from 'src/app/core/models/store.model';
+import { StoreService } from 'src/app/core/services/stores/store.service';
+import { __values } from 'tslib';
 
 interface SideNavToggle {
   screenWidth: number;
   collapsed: boolean;
 }
 
-interface FilteredOptions{
+interface FilteredOptions {
   id: number;
   name: string;
 }
@@ -20,48 +31,76 @@ interface FilteredOptions{
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StoresComponent {
+export class StoresComponent implements OnInit {
+  @Input() collapsed = false;
+
   title = 'planning';
 
   isSideNavCollapsed = false;
   screenwidth = 0;
+  stores: Array<Store> = [];
 
-  constructor(private _dialog: MatDialog,){}
+  //! Get all stores a veces tiene problemas para la carga y no se muestra la lista de tiendas
+  ngOnInit(): void {
+    this.getAllStores();
+  }
 
-  onToggleSideNav(event: SideNavToggle): void{
+  constructor(
+    private _dialog: MatDialog,
+    private cdRef: ChangeDetectorRef,
+    private storeService: StoreService
+  ) {}
+
+  onToggleSideNav(event: SideNavToggle): void {
     this.screenwidth = event.screenWidth;
     this.isSideNavCollapsed = event.collapsed;
   }
 
-  @Input() collapsed =false;
+  getAllStores() {
+    this.storeService.getAll().subscribe((response) => {
+      this.stores = response.registers;
+    });
+  }
 
-
-  getBodyClass(): string{
+  getBodyClass(): string {
     let styleClass = '';
-    if(this.collapsed && this.screenwidth > 768) {
+    if (this.collapsed && this.screenwidth > 768) {
       styleClass = 'body-trimmed';
-    }else if (this.collapsed && this.screenwidth <= 768 && this.screenwidth > 0) {
-      styleClass = 'body-md-scree'
+    } else if (
+      this.collapsed &&
+      this.screenwidth <= 768 &&
+      this.screenwidth > 0
+    ) {
+      styleClass = 'body-md-scree';
     }
     return styleClass;
   }
 
-  selectOptionVal: number = 0;
-  selectTypeDocVal: number = 0;
-  tempOptions: Array<FilteredOptions> = [];
   searchTerm: string = '';
-  search: Subject<void> = new Subject<void>();
   maxLengthSearch: number = 15;
-  filterBy: Subject<string> = new Subject<string>();
+
+  searchStores(){
+    this.storeService.getAll(this.searchTerm).subscribe((response) => {
+      this.stores = response.registers;
+      this.cdRef.detectChanges();
+    });
+  }
 
   openAddEditEmpForm() {
-    const dialogRef = this._dialog.open(EmpAddEditComponent);
+    const dialogRef = this._dialog.open(EmpAddEditComponent, {
+      data: null,
+    });
     dialogRef.afterClosed().subscribe({
       next: (val) => {
         if (val) {
-          console.log(val);
+          this.stores.push(val);
+          this.cdRef.detectChanges();
         }
       },
     });
+  }
+
+  deleteStore(store: Store) {
+    this.stores = this.stores.filter((s) => s.id !== store.id);
   }
 }
